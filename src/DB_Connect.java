66b6fc44d323aa47c23f31de00d24751d966c4e4
Yatochi <aqildanial.17@gmail.com>
2024-11-23 13:31:09 +0800

    init commit

---
+++ src/DB_Connect.java
@@ -0,0 +1,15 @@
+package com.library;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DB_Connect {
+    private static final String URL = "jdbc:mysql://localhost:3306/library_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "#17July2003";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
